8395962f34d49da665d2c26444da15c9d6dddd34
djunga <tora.mullings@stonybrook.edu>
2020-06-17 23:20:15 -0400

    Button can move once

--- CatchMe/CatchMe/ViewController.swift
+++ CatchMe/CatchMe/ViewController.swift
@@ -10,7 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     @IBOutlet weak var mainButton: UIButton!
+    //var bt = CatchButton()
+    @IBAction func touchDownButton(_ sender: UIButton) {
+        sender.center = .init(x: 30, y: 40)
+//        mainButton!.xPos = Int.random(in: 1..<100)
+//        mainButton!.yPos = Int.random(in: 1..<100)
+    }
+    
+//    override func touchDown(touches: NSSet!, withEvent event: UIEvent!) {
+//        var touch : UITouch! = touches.anyObject() as UITouch
+//
+//    location = touch.locationInView(self.view)
+//
+//    Person.center = location
+//}
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
