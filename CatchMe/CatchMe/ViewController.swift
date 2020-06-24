ff0c60bfaa70b7b4c741bd609fa353d73d224d6d
djunga <tora.mullings@stonybrook.edu>
2020-06-23 20:32:19 -0400

    A label to keep score was added

--- CatchMe/CatchMe/ViewController.swift
+++ CatchMe/CatchMe/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet weak var playAgainButton: UIButton!
     
     @IBOutlet weak var cageButton: UIButton!
     
@@ -22,52 +25,59 @@ class ViewController: UIViewController {
     var hamY:Int = 0
     var cageX:Int = 0
     var cageY:Int = 0
+    var score:Int = 0
+    
+    @IBAction func playAgainTrigger(_ sender: UIButton) {
+        playAgainButton.isHidden = true
+        hamX = Int.random(in: 80..<250)
+        hamY = Int.random(in: 80..<500)
+        cageX = hamX + Int.random(in: 60..<90) //hamX + 90
+        cageY = hamY + Int.random(in: 90..<150) //hamY + 80
+        mainButton.center = .init(x: hamX, y: hamY)
+        cageButton.center = .init(x: cageX, y: cageY)
+        buttoncoordinatesLabel.isHidden = true
+        mainButton.isEnabled = true
+        score = 0
+        scoreLabel.text = "Score: 0"
+    }
     
     @IBAction func touchDownButton(_ sender: UIButton) {
         hamX = Int.random(in: 30..<350)
         hamY = Int.random(in: 30..<630)
         sender.center = .init(x: hamX, y: hamY)
-        buttoncoordinatesLabel.text = "X: \(hamX), Y: \(hamY)"
-        //myButton.frame.size.height
+        //buttoncoordinatesLabel.text = "X: \(hamX), Y: \(hamY)"
         let hamWidth:Int = Int(mainButton.frame.size.width)
         let hamHeight:Int = Int(mainButton.frame.size.height)
         let cageWidth:Int = Int(cageButton.frame.size.width)
         let cageHeight:Int = Int(cageButton.frame.size.height)
 
-        //let mainRect = CGRect(x: hamX, y: hamY, width: hamWidth, height: hamHeight)
-        //let cageRect = CGRect(x: cageX, y: cageY, width: cageWidth, height: cageHeight)
-        var hamTLx = Int(hamX - hamWidth/2)
-        var hamTLy = Int(hamY - hamHeight/2)
-        var hamTL = CGPoint(x:hamTLx, y:hamTLy)
+        let hamTLx = Int(hamX - hamWidth/2)
+        let hamTLy = Int(hamY - hamHeight/2)
+        let hamTL = CGPoint(x:hamTLx, y:hamTLy)
         
-        var hamBRx = Int(hamX + hamWidth/2)
-        var hamBRy = Int(hamY + hamHeight/2)
-        var hamBR = CGPoint(x:hamBRx, y:hamBRy)
+        let hamBRx = Int(hamX + hamWidth/2)
+        let hamBRy = Int(hamY + hamHeight/2)
+        let hamBR = CGPoint(x:hamBRx, y:hamBRy)
         
-        var cageTLx = Int(cageX - cageWidth/2)
-        var cageTLy = Int(cageY - cageHeight/2)
-        var cageTL = CGPoint(x:cageTLx, y:cageTLy)
+        let cageTLx = Int(cageX - cageWidth/2)
+        let cageTLy = Int(cageY - cageHeight/2)
+        let cageTL = CGPoint(x:cageTLx, y:cageTLy)
         
-        var cageBRx = Int(cageX + cageWidth/2)
-        var cageBRy = Int(cageY + cageHeight/2)
-        var cageBR = CGPoint(x:cageBRx, y:cageBRy)
+        let cageBRx = Int(cageX + cageWidth/2)
+        let cageBRy = Int(cageY + cageHeight/2)
+        let cageBR = CGPoint(x:cageBRx, y:cageBRy)
+        
+        score = score + 1
+        scoreLabel.text = "Score: \(String(score))"
 
-//        if(!(hamTL.x >= cageBR.x || cageTL.x >= hamBR.x)) {
-//            buttoncoordinatesLabel.text = "You caught him!"
-//        }
-//        if(!(hamTL.y >= cageBR.y || cageTL.y >= hamBR.y)) {
-//            buttoncoordinatesLabel.text = "You caught him!"
-//        }
         if((hamTL.x >= cageTL.x && hamTL.x <= cageBR.x) && (hamTL.y <= cageBR.y && hamTL.y >= cageTL.y)) {
+            buttoncoordinatesLabel.isHidden = false
             buttoncoordinatesLabel.text = "You caught him!"
+            playAgainButton.isHidden = false
+            mainButton.isEnabled = false
+
         }
         
-
-//        if r1.x < l2.x no overlap
-//        if l1.y > r2.y no overlap
-//        if(mainRect.intersects(cageRect)) {
-//            buttoncoordinatesLabel.text = "You caught him!"
-//        }
     }
 
     override func viewDidLoad() {
@@ -77,8 +87,12 @@ class ViewController: UIViewController {
         cageY = hamY + Int.random(in: 90..<150) //hamY + 80
         mainButton.center = .init(x: hamX, y: hamY)
         cageButton.center = .init(x: cageX, y: cageY)
-        buttoncoordinatesLabel.text = "X: \(hamX), Y: \(hamY)"
-        
+        //buttoncoordinatesLabel.text = "X: \(hamX), Y: \(hamY)"
+        buttoncoordinatesLabel.isHidden = true
+        playAgainButton.isHidden = true
+        mainButton.isEnabled = true
+        score = 0
+        scoreLabel.text = "Score: 0"
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
